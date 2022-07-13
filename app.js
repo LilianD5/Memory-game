40237802dc1f8c4920ea0622fc1c1e3c6b896078
LilianD5 <97739931+LilianD5@users.noreply.github.com>
2022-07-13 16:38:27 +0200

    Memory is functionnal (cardflip, card comparison)

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-// Boardgame
+// Body
+const body = document.querySelector('body')
 
+// Boardgame
 const boardgame = document.getElementById('container')
 
-// Card Deck
-    // Deck Creation
-
+// Deck Creation
 function createCardsSet(){
     const cardsType = ['♟','♞','♝','♜','♛','♚']
     const cardDeck = cardsType.concat(cardsType)
@@ -14,16 +14,15 @@ function createCardsSet(){
     return cardDeck
 }
 
-    // Deck Shuffle
-
+// Deck Shuffle
 function shuffle(array){
     array.sort(() => Math.floor(Math.random() - 0.5))
 }
 
+let cardSet
 cardSet = createCardsSet()
 
 // Card Template
-
 function cardTemplate(cardValue){
     let card = document.createElement('div')
     let recto = document.createElement('div')
@@ -39,11 +38,107 @@ function cardTemplate(cardValue){
     boardgame.append(card)
 }
 
+// Card Creation
 function cardCreation(){
     cardSet.forEach(cardValue => {
         cardTemplate(cardValue)
-    });
+    })
 }
 
-cardCreation();
+cardCreation()
 
+// Card Flip
+function cardFlip(array){
+    array.forEach(element => {
+        element.classList.toggle('flipcard')
+    })
+}
+
+// Card Test Setup
+function cardTestSetup(element){
+    elementChild = element.childNodes
+    element.classList.toggle('pointer-none')
+    cardFlip(elementChild)
+}
+
+// Card Flip Back
+function cardFlipBack(element){
+    elementChild = element.childNodes
+                
+    elementChild.forEach(child => {
+        child.classList.toggle('flipcard')
+    })
+
+    element.classList.toggle('pointer-none')
+}
+
+// Victory Pop up 
+function popUp(){
+    let modal = document.createElement('div')
+    let congrats = document.createElement('p')
+
+    congrats.innerHTML = 'Congratulations !! You won !!'
+
+    modal.classList.add('popup')
+
+    body.prepend(modal)
+    modal.prepend(congrats)
+}
+
+// Card Comparison
+
+let arrayTest = []
+let arrayPairs = []
+
+const cards = Array.from(document.getElementsByClassName('card'))
+
+function cardComparison(){
+    cards.forEach(card => {
+
+        card.addEventListener('click', function () {
+            console.log(arrayPairs.length)
+            if(arrayPairs.length === 10){
+                cardTestSetup(card)
+                setTimeout(() => {
+                    popUp()
+                }, 1000);
+            } else {
+                
+                if (arrayTest.length < 2) {
+
+                    cardTestSetup(card)
+                    arrayTest.push(this)
+                    
+                } else if(arrayTest.length == 2){
+
+                    if (arrayTest[0].innerText !== arrayTest[1].innerText) {
+
+                        cardFlipBack(arrayTest[0])
+                        cardFlipBack(arrayTest[1])
+
+                        arrayTest = []
+
+                        cardTestSetup(card)
+                        arrayTest.push(this)
+
+                    } else {
+                        arrayPairs.push(arrayTest[0], arrayTest[1])
+
+                        arrayTest = []
+
+                        // console.log(arrayPairs.length);
+
+                        // if(arrayPairs.length === 12){
+                        //     popUp()
+                        // }
+
+                        cardTestSetup(card)
+                        arrayTest.push(this)
+                    }
+                }
+            }
+        })
+    })
+}
+
+cardComparison()
